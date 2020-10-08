a1e67857cc5f88cd8b25189319a009f092369d5a
mcalmette <mcalmette@gmail.com>
2020-10-07 20:56:29 -0700

    second

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 dateb = '20201001'
-CIK = '0000789019'
 form = '10-K'
 company_ticker = 'msft'
 endpoint = r"https://www.sec.gov/cgi-bin/browse-edgar"
@@ -22,7 +21,7 @@ if __name__ == '__main__':
                   'dateb':'20200913'}
     url = Get_URL.retrieve(endpoint,param_dict)
     #print(url)
-    secLink = Get_Data.sift(url,'2020') #has to be before 2020
+    secLink = Get_Data.sift(url,'2019')
     #seems to get funked up with the iXBRL
     print(secLink)
     doc_str = Get_Data.getHTML(secLink)
